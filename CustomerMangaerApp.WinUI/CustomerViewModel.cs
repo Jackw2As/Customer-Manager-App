2a4b56408410ce20333617949b9c60f5d5069274
Jackw2As <aaldersjack@gmail.com>
2021-12-22 10:00:10 +0000

    Intergrated .ViewModel into WInUI.

---
+++ CustomerMangaerApp.WinUI/CustomerViewModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CustomerManagerApp.Backend.Service;
+using CustomerManagerApp.Backend.ValueObjects;
+
+namespace CustomerManagerApp.ViewModel
+{
+    public class CustomerViewModel : ViewModelBase
+    {
+        private CustomerValueObject customer { get; init; }
+        private DataService customerData { get; }
+
+        public CustomerViewModel(   CustomerValueObject Customer,
+                                    DataService DataService)
+        {
+            customer = Customer;
+            customerData = DataService;
+        }
+
+        public string FirstName 
+        { 
+            get => customer.FirstName; 
+            set
+            {
+                if(value != null && value != customer.FirstName)
+                {
+                    customer.FirstName = value;
+                    PropertyHasChanged();
+                    PropertyHasChanged(nameof(DisplayName));
+                }
+            } 
+        }
+
+        public string LastName
+        {
+            get => customer.LastName;
+            set
+            {
+                if (value != null && value != customer.LastName)
+                {
+                    customer.LastName = value;
+                    PropertyHasChanged();
+                    PropertyHasChanged(nameof(DisplayName));
+                }
+            }
+        }
+
+        public bool IsDeveloper
+        {
+            get => customer.IsDeveloper;
+            set
+            {
+                if (value != customer.IsDeveloper)
+                {
+                    customer.IsDeveloper = value;
+                    PropertyHasChanged();
+                    PropertyHasChanged(nameof(CanSave));
+                }
+            }
+        }
+
+        public DateTimeOffset FirstTime
+        {
+            get => customer.FirstTime;
+            set
+            {
+                if (value != customer.FirstTime)
+                {
+                    customer.FirstTime = value;
+                    PropertyHasChanged();
+                    PropertyHasChanged(nameof(CanSave));
+                }
+            }
+        }
+
+        public string DrinkId
+        {
+            get => customer.DrinkID;
+            set
+            {
+                if (value != null && value != DrinkId)
+                {
+                    customer.DrinkID = value;
+                }
+            }
+        }
+
+        public string DisplayName { get => FirstName + " " + LastName; }
+
+        public bool CanSave { 
+            get 
+            {
+                if (IsSaving == false)
+                {
+                    //Model Validation Logic
+                    if (!string.IsNullOrWhiteSpace(FirstName))
+                    {
+                        if (!string.IsNullOrWhiteSpace(LastName))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+
+        public bool IsSaving { get; private set; } = false;
+
+        internal async void Remove()
+        {
+            await customerData.RemoveCustomerAsync(customer);
+        }
+
+        public async void SaveCustomer()
+        {
+            IsSaving = true;
+
+            await customerData.AddCustomerAsync(customer);
+
+            IsSaving = false;
+        }
+    }
+}
---
+++ CustomerMangaerApp.WinUI/ListViewModel.cs
@@ -0,0 +1,110 @@
+﻿using CustomerManagerApp.Backend.Repository.Drink;
+using CustomerManagerApp.Backend.Service;
+using CustomerManagerApp.Backend.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagerApp.ViewModel
+{
+    public delegate void RefreshEvent();
+    public delegate void CustomerChangedEvent(CustomerViewModel customer);
+    public class ListViewModel : ViewModelBase
+    {
+        public ObservableCollection<CustomerViewModel> Customers { get; } = new();
+        public ObservableCollection<DrinkValueObject> DrinkTypes { get; } = new();
+        private DataService Data { get; }
+
+        
+
+        public event RefreshEvent OnRefreshRaised;
+        public void RefreshList()
+        {
+            OnRefreshRaised?.Invoke();
+        }
+
+        /// <summary>
+        /// Called when a new customer is selected
+        /// </summary>
+        public event CustomerChangedEvent SelectedCustomerRaisedEvent;
+        public CustomerViewModel SelectedCustomer
+        {
+            get => selectedCustomer; 
+            set
+            {
+                if(selectedCustomer != value)
+                {
+                    selectedCustomer = value;
+                    SelectedCustomerRaisedEvent.Invoke(selectedCustomer);
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        public ListViewModel(DataService DataService)
+        {
+            Data = DataService;
+        }
+        public void CustomerAdd()
+        {
+            CustomerValueObject customer = new("new customer", "", DrinkTypes[0].Id);
+            var defualtCustomer = new CustomerViewModel(customer, Data);
+            Customers.Add(defualtCustomer);
+        }
+
+        public void CustomerRemove(CustomerViewModel customer)
+        {
+            Customers.Remove(customer);
+        }
+
+        public ObservableCollection<CustomerViewModel> CustomerList()
+        {
+            return Customers;
+        }
+
+        public string FilterValue = "";
+        public async void Filter()
+        {
+            foreach (var customer in Customers)
+            {
+                customer.SaveCustomer();
+            }
+            var customers = await Data.GetCustomersAsync();
+
+            Customers.Clear();
+            filteredList.Clear();
+            Parallel.ForEach(customers, customer => FilterByName(FilterValue, customer));
+
+            foreach (var item in filteredList)
+            {
+                Customers.Add(item);
+            }
+
+        }
+        private List<CustomerViewModel> filteredList = new();
+
+
+
+        private void FilterByName(string FilterText, CustomerValueObject customer)
+        {
+            if (customer.FirstName.Contains(FilterText) == false && customer.LastName.Contains(FilterText) == false)
+            {
+                return;
+            }
+
+            var model = new CustomerViewModel(customer, Data);
+            if (!filteredList.Contains(model))
+            {
+                filteredList.Add(model);
+            }
+        }
+
+        
+        private CustomerViewModel selectedCustomer;
+        
+
+    }
+}
