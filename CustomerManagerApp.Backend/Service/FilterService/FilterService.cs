5d6f3134d868aa32969439e98836585ceb77e3ee
Jackw2As <aaldersjack@gmail.com>
2022-03-06 17:04:48 +0000

    Extracted Filter to FilterService.

---
+++ CustomerManagerApp.Backend/Service/FilterService/FilterService.cs
@@ -0,0 +1,36 @@
+﻿using CustomerManagerApp.Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagerApp.Backend.Service.FilterService
+{
+    public class FilterService : IFilterService
+    {
+        private List<CustomerEntity> filteredList = new();
+        public List<CustomerEntity> FilterCustomerList(List<CustomerEntity> listToFilter, string filterValue)
+        {
+            filteredList.Clear();
+
+            //Nothing to Filter
+            if (filterValue == string.Empty)
+            {
+                return listToFilter;
+            }
+
+            Parallel.ForEach(listToFilter, customer => FilterByName(filterValue.ToLower(), customer));
+            return filteredList;
+        }
+        private void FilterByName(string FilterText, CustomerEntity customer)
+        {
+            if (customer.FirstName.ToLower().Contains(FilterText) == false
+                && customer.LastName.ToLower().Contains(FilterText) == false)
+            {
+                return;
+            }
+            filteredList.Add(customer);
+        }
+    }
+}
---
+++ CustomerManagerApp.Backend/Service/FilterService/IFilterService.cs
@@ -0,0 +1,14 @@
+﻿using CustomerManagerApp.Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagerApp.Backend.Service.FilterService
+{
+    public interface IFilterService
+    {
+        public List<CustomerEntity> FilterCustomerList(List<CustomerEntity> listToFilter, string filterValue);
+    }
+}
